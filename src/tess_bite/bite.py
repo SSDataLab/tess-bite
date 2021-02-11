a4fad3205d05e53c6ab31d2cc33d3400438b5cc1
Geert Barentsen <hello@geert.io>
2021-02-11 12:11:53 -0800

    :art: Black code formatting

--- src/tess_bite/bite.py
+++ src/tess_bite/bite.py
@@ -17,15 +17,20 @@ def bite_header():
 def bite(target, sector=None, shape=(5, 5)):
     """Returns a target pixel file."""
     from lightkurve.targetpixelfile import TargetPixelFileFactory
+
     crd = locate(target=target, sector=sector)[0]
     n_cadences = 1
     urls = [crd.get_images()[idx].url for idx in range(n_cadences)]
-    factory = TargetPixelFileFactory(n_cadences=n_cadences, n_rows=shape[1], n_cols=shape[0])
+    factory = TargetPixelFileFactory(
+        n_cadences=n_cadences, n_rows=shape[1], n_cols=shape[0]
+    )
     for idx, url in enumerate(urls):
         flux = bite_ffi(url, col=crd.column, row=crd.row, shape=shape)
         factory.add_cadence(idx, flux=flux)
-    tpf = factory.get_tpf(hdu0_keywords={"TELESCOP": "TESS", "CREATOR": "TessTargetPixelFile"})
-    tpf.quality_mask = [True]*n_cadences
-    tpf.get_header(1)['1CRV5P'] = 0
-    tpf.get_header(1)['2CRV5P'] = 0
+    tpf = factory.get_tpf(
+        hdu0_keywords={"TELESCOP": "TESS", "CREATOR": "TessTargetPixelFile"}
+    )
+    tpf.quality_mask = [True] * n_cadences
+    tpf.get_header(1)["1CRV5P"] = 0
+    tpf.get_header(1)["2CRV5P"] = 0
     return tpf
