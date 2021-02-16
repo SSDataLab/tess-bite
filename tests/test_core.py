5ecc3b9157bfbe81fb0b06e26623e670efa956b8
Geert Barentsen <hello@geert.io>
2021-02-15 19:15:15 -0800

    :white_check_mark: Fix first functional test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,34 +1,30 @@
 from astropy.io import fits
+import numpy as np
 
 from tess_bite import RemoteTessImage
 
 
-def test_basics():
-    with open("example.fits", "rb") as fp:
-        img = RemoteTessImage(url=fp)
-        fts = fits.open("example.fits")
-        assert img._download_range(0, 5).decode("ascii") == "SIMPLE"
-        assert img._download_range_multiple([(0, 5), (8, 8)]) == [b"SIMPLE", b"="]
-        assert img._find_data_offset(ext=0) == 2880
-        assert img._find_data_offset(ext=1) == 23040
-        assert img._find_pixel_offset(col=0, row=0) == 23040
-        assert img._find_pixel_range(col=0, row=0, shape=(1, 1)) == [(23040, 23043)]
-        assert (
-            img.download_cutout(col=0, row=0, shape=(1, 1))[0][0] == fts[1].data[0, 0]
-        )
-        assert (
-            img.download_cutout(col=1, row=0, shape=(3, 1)) == fts[1].data[0, (0, 1, 2)]
-        ).all()
-        # shape mismatch: assert (img.download_cutout(col=1, row=1, dcol=1, drow=3) == fts[1].data[(1, 2, 3), 1]).all()
-
-
-def test_basics2():
-    # Remote tests
+def test_remotetessimage_basics():
+    """Test basic features of RemoteTessImage."""
     url = "https://mast.stsci.edu/portal/Download/file?uri=mast:TESS/product/tess2019142115932-s0012-2-1-0144-s_ffic.fits"
     img = RemoteTessImage(url=url)
+    # Can we retrieve the first FITS header keyword? (SIMPLE)
     assert img._download_range(0, 5).decode("ascii") == "SIMPLE"
     assert img._download_range_multiple([(0, 5), (8, 8)]) == [b"SIMPLE", b"="]
-    assert img.download_cutout(col=0, row=0, shape=(1, 1))[0][0] == fts[1].data[0, 0]
+    # Can we find the correct start position of the data for extenions 0 and 1?
+    assert img._find_data_offset(ext=0) == 2880
+    assert img._find_data_offset(ext=1) == 23040
+    assert img._find_pixel_offset(col=0, row=0) == 23040
+    assert img._find_pixel_range(col=0, row=0, shape=(1, 1)) == [(23040, 23043)]
+    # Corner pixel
+    assert img.download_cutout_array(col=0, row=0, shape=(1, 1)).round(7) == 0.0941298
+    # First three pixels of the first row
     assert (
-        img.download_cutout(col=1, row=1, shape=(3, 1)) == fts[1].data[1, (0, 1, 2)]
+        img.download_cutout_array(col=1, row=0, shape=(3, 1)).round(7)
+        == np.array([0.0941298, -0.0605419, 0.0106343])
+    ).all()
+    # First three pixels of the first column
+    assert (
+        img.download_cutout_array(col=1, row=1, shape=(1, 3)).round(7)
+        == np.array([[-0.0605419], [0.0327947], [-0.0278026]])
     ).all()
